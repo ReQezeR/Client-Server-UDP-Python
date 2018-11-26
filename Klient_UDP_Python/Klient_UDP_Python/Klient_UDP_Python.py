5b8553c8de86f3aa87951daaaf1a83722ab16c19
ReQezeR <mp62639@o2.pl>
2018-11-26 20:32:23 +0100

    Poprawiona struktura Klienta i Servera

--- Klient_UDP_Python/Klient_UDP_Python/Klient_UDP_Python.py
+++ Klient_UDP_Python/Klient_UDP_Python/Klient_UDP_Python.py
@@ -1,44 +1,67 @@
+######################CLIENT#########################
 import socket
 import time
 import protocol
 from _thread import *
  
- 
- 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- 
 dstHost = ("192.168.1.84", 65432)
  
-client_ID = -1
+client_ID = 0
+print("Zadanie polaczenia z serwerem")
+client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "CONNECT", 0, client_ID).encode('utf-8'), dstHost)
+received_message1 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+client.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "REQUEST", 0, client_ID).encode(('utf-8')),dstHost)
+received_message2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+client.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()),0, client_ID, "").encode(('utf-8')),dstHost)
+received_message3 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+ 
+received_message4 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+client_ID = received_message4["id"]
+client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0 , client_ID).encode("utf-8"), dstHost)
+received_message5 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0 , client_ID).encode("utf-8"),dstHost)
+received_message6 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0 , client_ID).encode("utf-8"),dstHost)
+print("Poprawnie polaczono z serwerem")
+print("Otrzymane id:{}".format(client_ID))
+print("Oczekiwanie na drugiego klienta")
 
-client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()),"CONNECT",0,client_ID).encode('utf-8'),dstHost)
  
-received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
- 
-client_ID = received_message["id"]  # Przypisanie nadanego ID
  
 def send_message():
     message = input()
     if len(message) != 0:
+        client.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()), 0, client_ID, message).encode('utf-8'),dstHost)
+        # print("Send: ", message)
  
-        client.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()), 0, client_ID, message).encode('utf-8'),
-                  dstHost)
-        #print("Send: ", message)
  
 def recv_message():
     while True:
+
         received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
- 
-        if received_message["operacja"] == "ACK":
- 
-            #print(" [ send success ]")
+        client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0 , client_ID).encode("utf-8"), dstHost)
+
+        if received_message["operacja"] == "INFO":
+            received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+            client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0, client_ID).encode("utf-8"),dstHost)
+
+            if received_message["status"] == "INVITATIONS_ACTIVE":
+                received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+                client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0, client_ID).encode("utf-8"),dstHost)
+                print("Drugi klient jest podlaczony, mozesz go zaprosic (INVITE)")
+
+        elif received_message["operacja"] == "ACK":
             flaga = True
- 
+
         else:
-            print("[ "+received_message["id"] + " ]> " + received_message["data"])
- 
- 
+            print(received_message)
+            #print("[ " + str(received_message["id"]) + " ]> " + str(received_message["data"]))
+
+         
 while True:
-    start_new_thread(send_message,())
-    start_new_thread(recv_message,())
+    start_new_thread(send_message, ())
+ 
+    start_new_thread(recv_message, ())
+ 
     time.sleep(3)
--- Serwer_UDP_Python/Serwer_UDP_Python/Serwer_UDP_Python.py
+++ Serwer_UDP_Python/Serwer_UDP_Python/Serwer_UDP_Python.py
@@ -24,6 +24,7 @@ class Klient():
 # Zmienne globalne : 
 
 tablica_klientow = {}
+addr = []
 licznik_id = 1
 #=========================================================
 # Funkcje : 
@@ -56,9 +57,6 @@ def wyslij_do_sesji(sock, adr_klienta, client_data):
 
 # Przeslanie potwierdzenia otrzymania komunikatu
 def send_ack(sock, raw_data, adr_klienta):
-    #d = {}
-    #data = raw_data.decode("utf-8")
-    #d = protocol.decode_message(data)
     ack_data = protocol.encode_messsage_Operacja(time.ctime(time.time()),"ACK", 0, tablica_klientow[adr_klienta].id).encode("utf-8") # Tu wywala blad !!
     sent = sock.sendto(ack_data, tablica_klientow[adr_klienta].adres_surowy)
     return sent
@@ -69,8 +67,8 @@ def send_ack(sock, raw_data, adr_klienta):
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # Gniazdo
 
-server_address = '192.168.1.84'     # Adres IPv4 serwera
-server_port = 65432     # Port serwera
+server_address = '192.168.1.84'                             # Adres IPv4 serwera
+server_port = 65432                                         # Port serwera
 
 server = (server_address, server_port)
 sock.bind(server)
@@ -79,24 +77,85 @@ print("Listening on " + server_address + ":" + str(server_port))
 adr_klienta = ""
 pakiet = {}
 
+
+def client_connect():
+    global addr
+    client_data, client_address = sock.recvfrom(1024)  # Odebranie komunikatu
+    adr_klienta = adr_to_klucz(client_address[0], client_address[1])  # Klucz klienta ("IPv4:Port")
+
+    pakiet1 = protocol.decode_message(client_data.decode("utf-8"))  # Odkodowanie komunikatu
+    
+
+    if pakiet1["operacja"] == "CONNECT":
+        dodaj_klienta(adr_klienta, client_address)
+        pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0, pakiet1["id"]).encode("utf-8")
+        sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy)
+        client_data, client_address = sock.recvfrom(1024)
+        pakiet2 = protocol.decode_message(client_data.decode("utf-8"))
+
+        if pakiet2["status"] == "REQUEST":
+            pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0, pakiet2["id"]).encode("utf-8")
+            sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy)
+            client_data, client_address = sock.recvfrom(1024)
+            pakiet3 = protocol.decode_message(client_data.decode("utf-8"))
+            pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", 0, pakiet3["id"]).encode("utf-8")
+            sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy)
+            sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "CONNECT", 0, tablica_klientow[adr_klienta].id).encode("utf-8"),tablica_klientow[adr_klienta].adres_surowy)
+            client_data, client_address = sock.recvfrom(1024)
+            pakiet4 = protocol.decode_message(client_data.decode("utf-8"))
+            sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "CONNECTED", 0, tablica_klientow[adr_klienta].id).encode("utf-8"), tablica_klientow[adr_klienta].adres_surowy)
+            client_data, client_address = sock.recvfrom(1024)
+            pakiet5 = protocol.decode_message(client_data.decode("utf-8"))
+            sock.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()),0,tablica_klientow[adr_klienta].id,"").encode("utf-8"),tablica_klientow[adr_klienta].adres_surowy)
+            client_data, client_address = sock.recvfrom(1024)
+            pakiet5 = protocol.decode_message(client_data.decode("utf-8"))
+            addr.append(adr_klienta)
+            print("Nowy klient!")
+
+    elif pakiet1["operacja"]==0 and pakiet1["status"]==0:
+        print("[ " + pakiet1["id"] + " ] " + pakiet1["data"])
+
+#informowanie że jest dwóch klientów i mogą wysłać zaproszenie do komunikacji
+def info(addr):
+    if len(tablica_klientow)>=2:
+        sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "INFO", 0,tablica_klientow[addr].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
+        client_data, client_address = sock.recvfrom(1024)
+        sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "INVITATIONS_ACTIVE", 0, tablica_klientow[addr].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
+        client_data, client_address = sock.recvfrom(1024)
+        sock.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()), 0,tablica_klientow[addr].id, "").encode("utf-8"),tablica_klientow[addr].adres_surowy)
+        client_data, client_address = sock.recvfrom(1024)
+
+
+
 while True:
-    client_data, client_address = sock.recvfrom(1024)   # Odebranie komunikatu
-
-    adr_klienta = adr_to_klucz(client_address[0],client_address[1]) # Klucz klienta ("IPv4:Port")
-
-    pakiet = protocol.decode_message(client_data.decode("utf-8"))   # Odkodowanie komunikatu
-
-    if pakiet["operacja"] == "CONNECT" and pakiet["id"] == "-1": 
-        print("Nowy klient!")
-        dodaj_klienta(adr_klienta,client_address)
-        sent = send_ack(sock, client_data, adr_klienta)
-    else:
-        send_ack(sock, client_data, adr_klienta)
-        #wyslij_do_sesji(sock, adr_klienta, client_data)
-        print("[ "+pakiet["id"]+" ] "+pakiet["data"])
-         
+    client_connect()
+    if len(addr) >= 2:
+        info(addr[0])
+        info(addr[1])
 
 print("KONIEC!")
 
-#=========================================================    
-    
+# =========================================================  
+i=1
+if i == 0:
+    client_data, client_address = sock.recvfrom(1024)  # Odebranie komunikatu
+
+    adr_klienta = adr_to_klucz(client_address[0], client_address[1])  # Klucz klienta ("IPv4:Port")
+
+    pakiet = protocol.decode_message(client_data.decode("utf-8"))  # Odkodowanie komunikatu
+
+    if pakiet["operacja"] == "CONNECT" and pakiet["id"] == "-1":
+
+        print("Nowy klient!")
+
+        dodaj_klienta(adr_klienta, client_address)
+
+        sent = send_ack(sock, client_data, adr_klienta)
+
+    else:
+
+        send_ack(sock, client_data, adr_klienta)
+
+        # wyslij_do_sesji(sock, adr_klienta, client_data)
+
+        print("[ " + pakiet["id"] + " ] " + pakiet["data"])
