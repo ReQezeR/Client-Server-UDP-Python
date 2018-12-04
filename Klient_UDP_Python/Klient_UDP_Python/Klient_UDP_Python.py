f6743c29dc57476aaf4e6e8a74d1612416d82c44
ReQezeR <mp62639@o2.pl>
2018-12-04 19:58:58 +0100

    Działa WSZYSTKO

--- Klient_UDP_Python/Klient_UDP_Python/Klient_UDP_Python.py
+++ Klient_UDP_Python/Klient_UDP_Python/Klient_UDP_Python.py
@@ -4,20 +4,26 @@ import time
 import sys
 import threading
 import protocol
+import queue
+import re
+import os
+import signal
 from _thread import *
 
-#=========================================================
+import msvcrt
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dstHost = ("127.0.0.1", 65432)
  
-
-numer_sekwencyjny = 100
+numer_sekwencyjny = 0
 client_ID = 0
 flaga_odpowiedzi_na_invite = False
 flaga_rozlaczenia = False # Flaga kontrolująca poprawne DISCONNECT
-koniec_czekania = False 
+flaga_otrzymania_invite = False # Flaga kontrolująca otrzymanie INVITE
+flaga_wyslania_invite = False # Flaga kontrolująca wyslanie INVITE
 
 wiadomosc_temp=""
+#=========================================================
 #=========================================================
 # Funkcje potrzebne do INVITE>REQUEST
 
@@ -37,7 +43,7 @@ def send_invite_accept():
     client.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "ACCEPT", numer_sekwencyjny, client_ID).encode(('utf-8')),dstHost)
 
     received_message2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))# ACK RECV
-    print("wyslano akceptacje")
+    print("Wyslano potwierdzenie")
 
 def send_invite_denied():
     numer_sekwencyjny = 2
@@ -46,15 +52,66 @@ def send_invite_denied():
     client.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "DENY", numer_sekwencyjny, client_ID).encode(('utf-8')),dstHost)
 
     received_message2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))# ACK RECV
-    print("wyslano odmowe")
+    print("Wyslano odmowe")
+
+
+def czy_zaproszenie():
+    global flaga_otrzymania_invite
+    global flaga_wyslania_invite 
+    global wiadomosc_temp
+    while True:
+        if flaga_wyslania_invite == True:
+            return 0
+        else:
+            try:
+                recv_data = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+                print("Otrzymano INVITE od Klienta {}".format(recv_data["id"]))
+                client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny , client_ID).encode("utf-8"), dstHost)
+                wiadomosc_temp = recv_data
+                flaga_otrzymania_invite = True
+                return 0
+            except:
+                continue
+           
+# Funkcja czekajaca az klient wpisze INVITE
+def readInput(timeout = 20):
+    caption = ""
+    default = ""
+    start_time = time.time()
+    input = ''
+    while True:
+        if msvcrt.kbhit():
+            byte_arr = msvcrt.getche()
+            if ord(byte_arr) == 13: # enter_key
+                break
+            elif ord(byte_arr) >= 32: #space_char
+                input += "".join(map(chr,byte_arr))
+        if len(input) == 0 and (time.time() - start_time) > timeout:
+            #print("timing out")
+            break
+        elif flaga_otrzymania_invite == True:
+            break
+        elif flaga_wyslania_invite == True:
+            break
+
+    print('',end='')  # needed to move to next line
+    if len(input) > 0:
+        return input
+    else:
+        return default
+
 
 #=========================================================
 # Funkcje potrzebne do DISCONNECT
 
 def close_connection():
     print("Proba rozlaczenia: disconnect")
+    numer_sekwencyjny = 2
     client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "DISCONNECT", numer_sekwencyjny, client_ID).encode('utf-8'), dstHost)
-    received_message1 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+    numer_sekwencyjny -= 1
+    client.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "REQUEST", numer_sekwencyjny, client_ID).encode('utf-8'), dstHost)
+
+    received_message1 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))# ACK RECV
     print("Polaczenie rozwiazane!")
 
 #=========================================================
@@ -63,15 +120,15 @@ def close_connection():
 def send_data(message):
     numer_sekwencyjny = 3
     client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "COMMUNICATE", numer_sekwencyjny, client_ID).encode('utf-8'), dstHost)
-    print("wysylam COMMUNICATE")
+    #print("wysylam COMMUNICATE")
     numer_sekwencyjny -= 1
     client.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "SENT", numer_sekwencyjny, client_ID).encode(('utf-8')),dstHost)
-    print("wysylam SENT")
+    #print("wysylam SENT")
     numer_sekwencyjny -= 1
     client.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()),numer_sekwencyjny, client_ID, message).encode(('utf-8')),dstHost)
-    print("wysylam wiadomosc : {}".format(message))
+    #print("wysylam wiadomosc : {}".format(message))
     #received_message2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))# ACK
-    print("wyslano wiadomosc")
+    #print("wyslano wiadomosc")
 
 #=========================================================
 # Wysłanie sekwencji CONNECT>REQUEST>""
@@ -98,41 +155,41 @@ print("Oczekiwanie na drugiego klienta")
 while True:
     received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
 
-    #client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK",numer_sekwencyjny , client_ID).encode("utf-8"), dstHost)
     if received_message["operacja"] == "INFO":
         received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-        #client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
-        #numer_sekwencyjny+=1
-
         if received_message["status"] == "INVITATIONS_ACTIVE":
-            #received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-            numer_sekwencyjny+=1
+            numer_sekwencyjny = 1
             client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
             
-            print("Drugi klient jest podlaczony, mozesz go zaprosic (INVITE/ '' )")
-            
+            print("Klient {} jest podlaczony, mozesz go zaprosic (INVITE)".format(received_message["id"]))
+ 
+            client.settimeout(4)
+            start_new_thread(czy_zaproszenie,())
             
             while True:
-                message = input()
-                if len(message) != 0:
-                    if message == "INVITE":
-                        send_invite()
-                        break
-                    else:
-                        print("Nie mozna przeprowadzic komunikacji!")
-                        continue
-                else:
-                    received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-                    client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny , client_ID).encode("utf-8"), dstHost)
+                time.sleep(1)
+                message =""
+                result1= re.findall('(\w*)',readInput())
+                for part in result1:
+                    if part =="INVITE":
+                        message = part
+                        #print(">"+message)
+                if message == "INVITE":
+                    flaga_wyslania_invite = True
+                    client.setblocking(True)
+                    send_invite()
                     break
 
+                elif flaga_otrzymania_invite == True:
+                    received_message = wiadomosc_temp
+                    client.setblocking(True)
+                    break
 
     if received_message["operacja"] == "INVITE":
         received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-        #client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
+        
 
         if received_message["status"] == "ACCEPT":
-            #received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
             numer_sekwencyjny = 1
             client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
             print("Zaakceptowano zaproszenie")
@@ -140,7 +197,6 @@ while True:
             break
 
         if received_message["status"] == "DENY":
-            #received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
             numer_sekwencyjny = 1
             client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
             print("Odrzucono zaproszenie")
@@ -148,21 +204,18 @@ while True:
             break
 
         if received_message["status"] == "REQUEST":
-            #received_message = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-            
-            
             print("Otrzymano zaproszenie do czatu: (ACCEPT / DENY)")
             while True:
                 message = input()
                 if message == "ACCEPT":
-                    print("proba accept")
                     numer_sekwencyjny = 1
                     client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
                     send_invite_accept()
                     flaga_odpowiedzi_na_invite =True
                     break
                 elif message == "DENY":
-                    print("proba denied")
+                    numer_sekwencyjny = 1
+                    client.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8"),dstHost)
                     send_invite_denied()
                     flaga_odpowiedzi_na_invite = False
                     break
@@ -183,10 +236,6 @@ def send_message():
             return 0
         else:
             send_data(message)
-            #client.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()), numer_sekwencyjny, client_ID, message).encode('utf-8'),dstHost)
-        # print("Send: ", message)
-
-
 #========================================================================
 
 def recv_message():
@@ -196,32 +245,27 @@ def recv_message():
         if flaga_rozlaczenia == False:
             while True:
                 recv_m1 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-                #print("1: "+str(recv_m1))
-                #print(recv_m1["operacja"])
 
-                #if received_message1["operacja"]== "COMMUNICATE":
-                if int(recv_m1["nr_sekwencyjny"])>1:
-
+                if int(recv_m1["nr_sekwencyjny"])>1 and recv_m1["operacja"] != "DISCONNECT":
                     recv_m2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-                    #print("2: "+str(recv_m2))
-                    #if str(received_message2["status"]) == "SENT":
+
                     if int(recv_m2["nr_sekwencyjny"])>1:
                         recv_m3 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
-
                         print("[ " + str(recv_m3["id"]) + " ]> " + str(recv_m3["data"]))
 
                         numer_sekwencyjny = 1
                         pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8") # Utworzenie pakietu ACK
                         client.sendto(pakiet_ack, dstHost) # ACK SEND
-                    else:
-                        print("XD")
-                # DO zmiany !!!!!!
-                elif recv_m1["operacja"]=="DISCONNECTED":
+
+                elif int(recv_m1["nr_sekwencyjny"])>1 and recv_m1["operacja"] == "DISCONNECT":
+                    recv_m2 = protocol.decode_message(client.recvfrom(1024)[0].decode("utf-8"))
+
+                    numer_sekwencyjny = 1
                     pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, client_ID).encode("utf-8") # Utworzenie pakietu ACK
                     client.sendto(pakiet_ack, dstHost) # Wysłanie ACK na DISCONNCONNECTED
-                    numer_sekwencyjny+=1
+                    
                     flaga_rozlaczenia = True
-                    print("Klient sie rozlaczyl")
+                    print("Partner sie rozlaczyl")
                     return 0
            
 
--- Serwer_UDP_Python/Serwer_UDP_Python/Serwer_UDP_Python.py
+++ Serwer_UDP_Python/Serwer_UDP_Python/Serwer_UDP_Python.py
@@ -56,13 +56,13 @@ def wyslij_do_sesji(sock, adr_klienta, client_data):
         if tablica_klientow[k].nr_sesji == sesja and tablica_klientow[k].id != tablica_klientow[adr_klienta].id:
             numer_sekwencyjny = 3
             sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "COMMUNICATE", numer_sekwencyjny, tablica_klientow[adr_klienta].id).encode("utf-8"),tablica_klientow[k].adres_surowy)
-            print("przesylam COMMUNICATE")
+            
             numer_sekwencyjny = 2
             sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "SENT", numer_sekwencyjny, tablica_klientow[adr_klienta].id).encode("utf-8"),tablica_klientow[k].adres_surowy)
-            print("przesylam SENT")
+           
             numer_sekwencyjny = 1
             sock.sendto(client_data, tablica_klientow[k].adres_surowy)
-            print("wysylam wiadomosc")
+           
             received_ack = protocol.decode_message(sock.recvfrom(1024)[0].decode("utf-8"))# ACK
 
 
@@ -79,7 +79,7 @@ def send_invite_accept(adres_surowy):
     sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "ACCEPT", numer_sekwencyjny, 1).encode(('utf-8')),adres_surowy)
 
     received_message2 = protocol.decode_message(sock.recvfrom(1024)[0].decode("utf-8"))# ACK
-    print("wyslano akceptacje")
+    print("[SERWER] Wyslano potwierdzenie")
 
 def send_invite_denied(adres_surowy):
     numer_sekwencyjny = 2
@@ -88,7 +88,7 @@ def send_invite_denied(adres_surowy):
     sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "DENY", numer_sekwencyjny, 1).encode(('utf-8')),adres_surowy)
 
     received_message2 = protocol.decode_message(sock.recvfrom(1024)[0].decode("utf-8"))# ACK
-    print("wyslano odmowe")
+    print("[SERWER] Wyslano odmowe")
 
 #=========================================================
 # MAIN :
@@ -100,7 +100,7 @@ server_port = 65432                                         # Port serwera
 
 server = (server_address, server_port)
 sock.bind(server)
-print("Listening on " + server_address + ":" + str(server_port))
+print("[SERWER] Listening on " + server_address + ":" + str(server_port))
 
 adr_klienta = ""
 pakiet = {}
@@ -122,10 +122,6 @@ def client_connect():
 
     if pakiet1["operacja"] == "CONNECT": # Jezeli operacja = CONNECT
         
-        #pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet1["id"]).encode("utf-8") # Utworzenie pakietu ACK
-        #sock.sendto(pakiet_ack, client_address) # Wysłanie ACK na CONNECT
-       
-       
         client_data, client_address = sock.recvfrom(1024) # Oczekiwanie na status REQUEST
         pakiet2 = protocol.decode_message(client_data.decode("utf-8")) # Odkodowanie pakietu
 
@@ -151,30 +147,36 @@ def client_connect():
 
      # Obsługa DISCONNECT
     elif pakiet1["operacja"] == "DISCONNECT": 
-        print("Otrzymano DISCONNECT")
-        pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")# ACK dla DISCONNECT
-        sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy) # Wyslanie ACK
-        numer_sekwencyjny+=1
+        print("[SERWER] Otrzymano DISCONNECT")
+        client_data, client_address = sock.recvfrom(1024) # Oczekiwanie na status
+        pakiet2 = protocol.decode_message(client_data.decode("utf-8")) # Odkodowanie pakietu
 
-        # Koniec obslugi klienta ktory wywolal CLOSE
-        print("Koniec obslugi klienta ktory wywolal CLOSE")
+        if pakiet2["status"] == "REQUEST":
+            pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")# ACK dla DISCONNECT
+            sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy) # Wyslanie ACK
 
-        #Przeslanie info o rozlaczeniu do drugiego klienta
-        for adres in addr:
-            if adres != adr_to_klucz(client_address[0],client_address[1]):
-                  adres_odbiorcy = tablica_klientow[adres].adres_surowy
+            # Koniec obslugi klienta ktory wywolal CLOSE
+            print("[SERWER] Koniec obslugi klienta ktory wywolal CLOSE")
 
-        pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "DISCONNECTED", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")
-        sock.sendto(pakiet_ack,adres_odbiorcy)# Wyslanie info o rozlaczeniu
-        pakiet4 = protocol.decode_message(client_data.decode("utf-8"))# Otrzymanie ACK
+            #Przeslanie info o rozlaczeniu do drugiego klienta
+            for adres in addr:
+                if adres != adr_to_klucz(client_address[0],client_address[1]):
+                        adres_odbiorcy = tablica_klientow[adres].adres_surowy
 
+            numer_sekwencyjny = 2
+            pakiet3 = protocol.encode_messsage_Operacja(time.ctime(time.time()), "DISCONNECT", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")
+            sock.sendto(pakiet3,adres_odbiorcy)# Wyslanie info o rozlaczeniu
+            numer_sekwencyjny -= 1
+            pakiet4 = protocol.encode_messsage_Status(time.ctime(time.time()), "TERMINATE", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")
+            sock.sendto(pakiet4,adres_odbiorcy)# Wyslanie info o rozlaczeniu
+
+            pakiet5 = protocol.decode_message(client_data.decode("utf-8"))# Otrzymanie ACK
+            print("[SERWER] Koniec obslugi drugiego klienta")
+            # Koniec obslugi drugiego klienta
 
     # Obsługa INVITE>REQUEST
     elif pakiet1["operacja"] == "INVITE":
-        
-        #pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet1["id"]).encode("utf-8") # Utworzenie pakietu ACK
-        #sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy ) # Wysłanie ACK na CONNECT
-        print("otrzymalem invite")
+        print("[SERWER] Otrzymano INVITE")
 
         client_data, client_address = sock.recvfrom(1024) # Oczekiwanie na status
         pakiet2 = protocol.decode_message(client_data.decode("utf-8")) # Odkodowanie pakietu
@@ -186,7 +188,7 @@ def client_connect():
                 adres_odbiorcy = tablica_klientow[adres_nadawcy].adres_surowy
 
         if pakiet2["status"] == "REQUEST":
-            print("otrzymalem request")
+            print("[SERWER] Otrzymano request")
             numer_sekwencyjny = 1
             pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet2["id"]).encode("utf-8")
             sock.sendto(pakiet_ack, client_address) # ACK SEND
@@ -198,24 +200,21 @@ def client_connect():
             print(adr_to_klucz(client_address[0],client_address[1]))
             print(adres_odbiorcy)
 
-            print("odebralem serie invite/request")
+            print("[SERWER] Odebrano serie invite/request")
 
 
             # Wysłanie serii komunikatow zapraszajacych do rozmowy
             numer_sekwencyjny = 2
             sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "INVITE", numer_sekwencyjny, tablica_klientow[adr_klienta].id).encode("utf-8"),adres_odbiorcy)
-            #client_data, client_address = sock.recvfrom(1024)
-            #pakiet4 = protocol.decode_message(client_data.decode("utf-8"))
+            
             sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "REQUEST", numer_sekwencyjny, tablica_klientow[adr_klienta].id).encode("utf-8"), adres_odbiorcy)
-            #client_data, client_address = sock.recvfrom(1024)
-            #pakiet5 = protocol.decode_message(client_data.decode("utf-8"))
-            #sock.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()),numer_sekwencyjny,tablica_klientow[adr_klienta].id,"").encode("utf-8"),adres_odbiorcy)
+           
             client_data, client_address = sock.recvfrom(1024)# ACK RECV
-            print("wyslalem invite request")
+            print("Wyslano invite/request")
             # Koniec serii
 
         elif pakiet2["status"] == "ACCEPT":
-            print("odebralem invite/ ACCEPT")
+            print("[SERWER] Odebrano INVITE/ ACCEPT")
             numer_sekwencyjny = 1
             pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet2["id"]).encode("utf-8")
             sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy)# ACK SEND
@@ -233,13 +232,9 @@ def client_connect():
                 elif tablica_klientow[adres].adres_surowy == client_address:
                     tablica_klientow[adres].nr_sesji=nsesji
             nsesji+=1
-            #print("FLAGA [0]")
-            #sock.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()),numer_sekwencyjny,tablica_klientow[adr_klienta].id,"ACCEPT").encode("utf-8"),tablica_klientow[adr_klienta].adres_surowy)
-            #client_data, client_address = sock.recvfrom(1024)
-            #print("FLAGA [1]")
 
         elif pakiet2["status"] == "DENY":
-            print("odebralem invite/ DENY")
+            print("[SERWER] Odebrano INVITE/ DENY")
             numer_sekwencyjny = 1
             pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet2["id"]).encode("utf-8")
             sock.sendto(pakiet_ack, tablica_klientow[adr_klienta].adres_surowy)# ACK SEND
@@ -256,18 +251,16 @@ def client_connect():
 
     elif pakiet1["operacja"]=="COMMUNICATE":
         #wyslij_do_sesji(sock, adr_klienta, client_data)
-        print("otrzymalem COMMUNICATE")
+        #print("Otrzymano COMMUNICATE")
         client_data, client_address = sock.recvfrom(1024) # Oczekiwanie na status REQUEST
         pakiet2 = protocol.decode_message(client_data.decode("utf-8")) # Odkodowanie pakietu
 
         if pakiet2["status"]=="SENT":
-            #wyslij_do_sesji(sock, adr_klienta, client_data)
-            print("otrzymalem SENT")
+            #print("Otrzymano SENT")
             client_data, client_address = sock.recvfrom(1024) # Oczekiwanie na status REQUEST
             pakiet3 = protocol.decode_message(client_data.decode("utf-8")) # Odkodowanie pakietu
-            print("otrzymalem wiadomosc: {}".format(pakiet3["data"]))
-
-            print("[ " + pakiet3["id"] + " ] " + pakiet3["data"])
+           
+            print("[  " + pakiet3["id"] + "  ] " + pakiet3["data"])
             for adres in tablica_klientow:
                 if adres == adres_nadawcy:
                     adres_odbiorcy = tablica_klientow[adres_nadawcy].adres_surowy
@@ -276,28 +269,23 @@ def client_connect():
             pakiet_ack = protocol.encode_messsage_Operacja(time.ctime(time.time()), "ACK", numer_sekwencyjny, pakiet1["id"]).encode("utf-8")
             sock.sendto(pakiet_ack, adres_odbiorcy)
 
-            wyslij_do_sesji(sock, adr_klienta, client_data)
+            wyslij_do_sesji(sock, adr_klienta, client_data)# Przesłanie komunikatu do docelowego uczestnika sesji
 
 #informowanie że jest dwóch klientów i mogą wysłać zaproszenie do komunikacji
-def info(addr):
+def info(addr,addr_partnera):
     if len(tablica_klientow)>=2:
         numer_sekwencyjny = 2
-        sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "INFO", numer_sekwencyjny,tablica_klientow[addr].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
-        #client_data, client_address = sock.recvfrom(1024)
+        sock.sendto(protocol.encode_messsage_Operacja(time.ctime(time.time()), "INFO", numer_sekwencyjny,tablica_klientow[addr_partnera].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
         numer_sekwencyjny = 1
-        sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "INVITATIONS_ACTIVE", numer_sekwencyjny, tablica_klientow[addr].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
+        sock.sendto(protocol.encode_messsage_Status(time.ctime(time.time()), "INVITATIONS_ACTIVE", numer_sekwencyjny, tablica_klientow[addr_partnera].id).encode("utf-8"),tablica_klientow[addr].adres_surowy)
         client_data, client_address = sock.recvfrom(1024)# ACK RECV
-
-        #sock.sendto(protocol.encode_messsage_Dane(time.ctime(time.time()), numer_sekwencyjny,tablica_klientow[addr].id, "").encode("utf-8"),tablica_klientow[addr].adres_surowy)
-        #client_data, client_address = sock.recvfrom(1024)
-
 
 kaszanka =1
 while True:
     client_connect()
     if len(addr) == 2 and kaszanka==1:
-        info(addr[0])
-        info(addr[1])
+        info(addr[0],addr[1])
+        info(addr[1],addr[0])
         kaszanka+=1
 
 print("KONIEC!")
@@ -320,7 +308,7 @@ if i == 0:
 
     if pakiet["operacja"] == "CONNECT" and pakiet["id"] == "-1":
 
-        print("Nowy klient!")
+        print("[SERWER] Nowy klient!")
 
         dodaj_klienta(adr_klienta, client_address)
 
